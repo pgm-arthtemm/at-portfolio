b7a77a5432012934f74714155a2cc01a428b572a
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2021-09-02 16:09:12 +0200

    finished navigation

--- src/components/layout/Nav.svelte
+++ src/components/layout/Nav.svelte
@@ -1,25 +1,21 @@
 <nav class="bg-transparent hidden md:block w-11/12 m-auto">
-  <div class="flex--container flex py-5 justify-between items-center">
-    <div class="heading flex items-center">
-      <div class="logo--wrapper">
-        <img class="w-14 mr-6" src="/ico.png" alt="logo">
+  <div class="flex py-5 justify-between items-center">
+    <a href="/">
+      <div class="flex items-center">
+        <div>
+          <img class="w-14 mr-6" src="/ico.png" alt="logo">
+        </div>
+        <h2 class="text-white font-bold text-2xl">Arthur <span class="text-red-700">Temmerman</span></h2>
       </div>
-      <h2 class="text-white">Arthur <span>Temmerman</span></h2>
-    </div>
+    </a>
     
     <div>
-      <ul class="nav--link__list flex items-center">
-        <li class="ml-12"><a href="/">Home</a></li>
-        <li class="ml-12"><a href="/projects">Projects</a></li>
-        <li class="ml-12"><a href="/info">Info</a></li>
-        <li class="ml-12"><a href="/contact">Contact</a></li>
+      <ul class="flex items-center">
+        <li class="transition duration-100 ease-in font-bold text-2xl ml-12 hover:text-red-700"><a href="/">Home</a></li>
+        <li class="transition duration-100 ease-in font-bold text-2xl ml-12 hover:text-red-700"><a href="/projects">Projects</a></li>
+        <li class="transition duration-100 ease-in font-bold text-2xl ml-12 hover:text-red-700"><a href="/info">Info</a></li>
+        <li class="transition duration-100 ease-in font-bold text-2xl ml-12 hover:text-red-700"><a href="/contact">Contact</a></li>
       </ul>
     </div>
   </div>
 </nav>
-
-<style>
-  span {
-    color: red;
-  }
-</style>
